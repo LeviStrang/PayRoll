e033e8a530881cc6a2e2b24679f920d51613c31b
Levi Strang <LeviStrang@outlook.com>
2018-10-30 19:50:41 -0400

    import project

---
+++ Pair Programming Using Collections/Employee.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pair_Programming_Using_Collections
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public double RateofPay { get; set; }
+        public double HoursWorked { get; set; }
+        //This constructor has all the arguments needed for the list.
+        public Employee(int EmpID, string EmpName, double ROPay, double Hours)
+        {
+            EmployeeID = EmpID;
+            EmployeeName = EmpName;
+            RateofPay = ROPay;
+            HoursWorked = Hours;
+        }
+        //each one of these variables are made public static to be used as a total of all the lists combined for each area. Having thembe static allows me to use them in the main method.
+        public static double RegularPayTotal = 0;
+        public static double OverTimeTotal = 0;
+        public static double Grandtotal = 0;
+
+
+        public double CalculateRegPay()
+        { //This method allows me to calculate the regular pay for each employ before overtime is added.
+            if (HoursWorked <= 40)
+            {
+                double RegPay = (HoursWorked * RateofPay);
+                return RegPay;
+            }
+            else
+            {
+                double RegPay = (40 * RateofPay);
+                return RegPay;
+            }
+        }
+        public double CalculateOvertime()
+        { //This method allows me to calculate all the overtime for each employee in the list. ONLY the over time.
+            if (HoursWorked > 40)
+            {
+                
+                double Overtime = (HoursWorked - 40) * (RateofPay * 1.5);
+                return Overtime;
+            }
+            else
+            {
+                double Overtime = 0;
+                return Overtime;
+            }
+        }
+
+
+        public double CalculatePay(double RateofPay, double HoursWorked)
+        {// this method calculates the total of both the overtime and regular pay for each employee in the list. It also uses += for the total regular pay, total overtime pay and grand total for all the employees.
+                if (HoursWorked <= 40)
+                {
+                    double RegPay = CalculateRegPay();
+                    Grandtotal += RegPay;
+                    RegularPayTotal += RegPay;
+                    return RegPay;
+
+                }
+
+                else
+                {
+                    //calculating Employees who worked more than 40 hours
+                    double OverPay = (CalculateOvertime() + CalculateRegPay());
+                    OverTimeTotal += CalculateOvertime();
+                    RegularPayTotal += CalculateRegPay();
+                    Grandtotal += OverPay;
+                    
+                    return OverPay;
+                }
+               
+            
+        }
+
+    }
+}
---
+++ Pair Programming Using Collections/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Pair_Programming_Using_Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //I am adding each employee to a new list. Each of the parameters passed in this new addition comes from the arguments specifed in the contructor of the Employee class.
+            List<Employee> employees = new List<Employee>
+            {
+                new Employee(223445, "John Landry", 15.00, 35.5),
+                new Employee(223476, "Angela Rush", 15.00, 32.25),
+                new Employee(223487, "Bill Marsh", 16.50, 45.5),
+                new Employee(223504, "Peggy Simon", 16.75, 50),
+                new Employee(223512, "Carl Yost", 15.75, 40),
+                new Employee(223519, "Joe Miles", 15.00, 40),
+                new Employee(223525, "Joan Jeffries", 14.25, 25.75),
+                new Employee(223536, "George Mills", 13.65, 38),
+                new Employee(223542, "Gary Cooper", 15.35, 37.25),
+                new Employee(223558, "Mary Millicent", 15.75, 40)
+            };
+
+
+
+            Console.WriteLine($"EmployeeID:     Employee Name:        Rate of Pay:     Hours Worked:            Regular Pay:           Overtime Pay:         Pay Total:");
+
+            foreach (var item in employees)
+            {
+               
+                Console.WriteLine
+                    (
+                    $"{item.EmployeeID, 10}"           +          
+                    $"{item.EmployeeName, 18}"         +    
+                    $"{item.RateofPay, 18:C}"          +     
+                    $"{item.HoursWorked, 17}"          +   
+                    $"{item.CalculateRegPay(), 26:C}"  + 
+                    $"{item.CalculateOvertime(), 23:C}"+ 
+                    $"{item.CalculatePay(item.RateofPay, item.HoursWorked), 22:C}"
+                    );
+                
+            }
+            Console.WriteLine($"\nRegular pay total: {Employee.RegularPayTotal:C}\n");
+            Console.WriteLine($"\nOvertime total: {Employee.OverTimeTotal:C}\n");
+            Console.WriteLine($"\nGrandtotal: {Employee.Grandtotal:C}\n");
+        }
+
+
+   
+  
+    }
+}
